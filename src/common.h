ca191bb7b156a88f4b2450ac74d8d69fe7e1fbe7
Andrei Tsurkan <andrey.turcan@hotmail.com>
2021-11-25 15:46:47 +0300

    Change source structure

---
+++ src/common.h
@@ -0,0 +1,12 @@
+#ifndef _INCLUDE_COMMON
+#define _INCLUDE_COMMON
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <math.h>
+
+#endif
--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-
-int main()
-{
-    printf("%s", "Hello world!\n");
-
-    return 0;
-}
